92c27af0e8f44f38e71814583720f033e9d983f9
kunalmkv <kunalm4.kvtagore@gmail.com>
2024-10-09 22:04:15 +0530

    require statement

---
+++ require.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract InputChecker {
+
+    // Function to check if the input is within the valid range (0 to 255)
+    function checkInput(uint input) public pure returns (string memory) {
+        require(input >= 0 && input <= 255, "Not Within Range");
+        return "Within Range";
+    }
+}
