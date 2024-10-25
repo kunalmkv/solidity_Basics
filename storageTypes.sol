de1b8dddd5a2c86c8f7e496528b843af93213364
kunalmkv <kunalm4.kvtagore@gmail.com>
2024-10-25 09:56:43 +0530

    storage | stable | compare memory and storage

---
+++ storageTypes.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract storageTypes {
+    constructor(){
+
+    }
+
+    uint [3] public fixedSizeArray = [1,2,3];
+
+    function storageArray() external {
+        uint[3] storage storageArray = fixedSizeArray;
+        storageArray[0] = 10;
+    }
+
+    function memoryArray() external {
+        uint[3] memory memoryArray = fixedSizeArray;
+        memoryArray[0] = 100;
+        return memoryArray;
+    }
+}
+
