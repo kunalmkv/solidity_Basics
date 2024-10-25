99a9e4731a94183abfb6f149024ec09d0dfdcaa4
kunalmkv <kunalm4.kvtagore@gmail.com>
2024-10-25 10:01:57 +0530

    practice | stable | polymorphism

---
+++ polymorphism.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+contract polymorphism {
+    constructor(){
+
+    }
+
+    function add(uint a, uint b) public pure returns(uint){
+        return a+b;
+    }
+
+    function add(uint a, uint b, uint c) public pure returns(uint){
+        return a+b+c;
+    }
+
+    function add(uint a, uint b, uint c, uint d) public pure returns(uint){
+        return a+b+c+d;
+    }
+
+}
