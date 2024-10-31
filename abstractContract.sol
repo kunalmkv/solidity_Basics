31f2d513dacb4f221f1781bf04e4b00de6937a67
kunalmkv <kunalm4.kvtagore@gmail.com>
2024-10-31 14:37:06 +0530

    abstract | stable | learn

---
+++ abstractContract.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.10;
+
+abstract contract abstractContract {
+    constructor(){
+str="Hello world";
+manager=msg.sender;
+    }
+    string public str;
+    address public manager;
+    function abstractFunc() public pure virtual returns(uint);
+}
+
+contract concreteContract is abstractContract{
+    constructor(){
+    }
+    function abstractFunc() public pure override returns(uint){
+        return 10;
+    }
+}
