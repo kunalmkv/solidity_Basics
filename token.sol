e38ed9263126641cdb4521ecd34283d10516a0f1
kunalmkv <kunalm4.kvtagore@gmail.com>
2025-02-15 11:03:39 +0530

    fixes | token

--- token.sol
+++ token.sol
@@ -3,12 +3,12 @@ pragma solidity ^0.8.20;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract GLDToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("decimal1", "DEC1") {
+contract Nexus is ERC20  {
+    constructor(uint256 initialSupply) ERC20("Nexus Coin", "NXC") {
         _mint(msg.sender, initialSupply);
     }
 
     function decimals() public view virtual override returns (uint8) {
-        return 1;
+        return 18;
     }
 }
--- votingContractWithToken.sol
+++ votingContractWithToken.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.10;
 
-import {IERC20} from "./irc20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+//import {IERC20} from "./irc20.sol";
 
 contract Vote {
     constructor(address _gldToken) {
